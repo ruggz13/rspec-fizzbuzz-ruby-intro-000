12870e896911b048c6d0e5ebe4e70f545838269d
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-04-30 21:27:26 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,6 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  end
-  if int % 5 == 0
-    puts "Buzz"
-  end
-  if int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
   else
     return nil
   end
